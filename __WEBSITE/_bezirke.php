6f5bfe5ed62efd05c12cbe3d577b0c91e93b8251
FlorianWiekhorst <florian.wiekhorst@haw-hamburg.de>
2017-06-17 14:21:48 +0200

    pushed changes

--- __WEBSITE/_bezirke.php
+++ __WEBSITE/_bezirke.php
@@ -1,7 +1,7 @@
 
 <!DOCTYPE html>
 <html lang="de">
-
+	
   <head>
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,13 +9,11 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 	<meta name"description" content="Districts of Hamburg">
 	<meta name"author" content="Florian Wiekhorst, Tien Huy Tonny Van">
-
+	
 	<title>Die Bezirke von Hamburg</title>
-
+	
 	<!-- Bootstrap core CSS -->
     <link href="dist/css/bootstrap.min.css" rel="stylesheet">
-	<!-- <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.4.min.js"></script> -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<!-- giev bootstrap icons -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
@@ -24,24 +22,24 @@
 
     <!-- Custom styles for this template -->
 	<link href="cover.css" rel="stylesheet">
-
+	
 	 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+    <![endif]-->    
+	
 	<!-- Einbinden der D3-lib -->
 	<script src="https://d3js.org/d3.v3.min.js"></script>
     <!-- Einbinden des Bootstrap-Stylesheets -->
     <link rel="stylesheet" href="https://ajax.aspnetcdn.com/ajax/bootstrap/3.3.6/css/bootstrap.min.css">
     <!-- optional: Einbinden der jQuery-Bibliothek -->
-
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.4.min.js"></script>
     <!-- optional: Einbinden der Bootstrap-JavaScript-Plugins -->
     <script src="https://ajax.aspnetcdn.com/ajax/bootstrap/3.3.6/bootstrap.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="CSS/main.css">
   </head>
-
+  
 <body>
 <h1>Die Bezirke von Hamburg</h1>
 <div id="parent">
@@ -66,13 +64,13 @@
                         .projection(projection);
 
             //Create SVG element
-
+            
 			var svg = d3.select("body")
                         .append("svg")
                         .attr("width", w)
                         .attr("height", h)
 						.attr("class", "areas");
-
+			
 
             //Load in GeoJSON data
             d3.json("Altona_AL9.GeoJson", function(json) {
@@ -82,7 +80,7 @@
                 s = 1 / Math.max((b[1][0] - b[0][0]) / w, (b[1][1] - b[0][1]) / h),
                 t = [(w - s * (b[1][0] + b[0][0])) / 2, (h - s * (b[1][1] + b[0][1])) / 2];
 
-                // Update the projection
+                // Update the projection    
                 projection
                   .scale(s)
                   .translate(t);
@@ -96,13 +94,13 @@
                    .style("fill", "#70bcbf")
 				   .attr("class", "bezirke")
 				   .attr("id", function(d) { return d.id; })
-				   .attr("name", function(d) {return d.name; });
+				   .attr("name", function(d) {return d.name; });   
             });
         </script>
-			</div>
-
+			</div>   
+		
 			<div id="narrow" class="col-sm-4"> <!-- Seitenbereich mit den Angaben -->
-
+			
 					<div id="werte">
 						<!-- Überschrift mit StadtteilNamen per Script ändern -->
 						<h3 id="stadtteilname">&nbsp;</h3>
@@ -112,7 +110,7 @@
 				<a href="vergleicher.php" class="btn btn-default btn-lg btn-block">Zum Stadtteilvergleicher</a>
 			</div>
 		</div>
-
+		
 		<!-- Einbinden der code.js -->
 		<script src="code.js"></script>
 	</body>
---
+++ __WEBSITE/code.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	$("#1,#2,#3,#4,#5,#6,#7").on("mouseenter",function(e) {
+		let myName = $(this).attr('name');
+		$("#stadtteilname").text(myName);
+    //
+		// var value = $(this).attr('id');
+		// $("#testme").text(value);
+    e.preventDefault();
+	});
+	$(".areas").on("mouseleave",function(){
+		$("#stadtteilname").text("\xA0");
+		$("#testme").text("\xA0");
+	});
+	$("#1,#2,#3,#4,#5,#6,#7").click(function(e) {
+		// alert("click");
+		let name = $(this).attr('name');
+		$.ajax({
+			url: 'bezirk_query.php',
+			data: "",
+			cache: false,
+			async: false,
+			dataType: 'json',
+			success: function(rows){
+
+				for (var i in rows)
+				{
+					// var row = rows[i];
+					// console.log(rows[0]);
+					// var obj = JSON.parse(rows[i]);
+
+					if(rows[i].Stadtteilname == name){
+
+						var id = rows[i].id;
+						var districtname = rows[i].Stadtteilname;
+						var population = rows[i].Bevölkerung;
+						$('#stadtteilname').text(id + " " + districtname + " " + population);
+					}
+
+				}
+			}
+		});
+		e.preventDefault();
+	});
+});
